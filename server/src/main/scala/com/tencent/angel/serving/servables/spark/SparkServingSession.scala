0b0bcf122be6f8e9f881ee2e7960ac9fd70c2cf7
fitzwang <fitzwang@tencent.com>
2019-04-03 16:20:19 +0800

    remove sparkserving

--- server/src/main/scala/com/tencent/angel/serving/servables/spark/SparkServingSession.scala
+++
@@ -1,7 +0,0 @@
-package com.tencent.angel.serving.servables.spark
-
-import com.tencent.angel.serving.servables.common.Session
-
-class SparkServingSession extends Session {
-
-}
--- server/src/main/scala/org/apache/spark/ml/transformer/ServingModel.scala
+++
@@ -1,8 +0,0 @@
-package org.apache.spark.ml.transformer
-
-import org.apache.spark.ml.param.ParamMap
-
-abstract class ServingModel[M <: ServingModel[M]] extends ServingTrans {
-
-  override def copy(extra: ParamMap): M
-}
